e9610cbf115ac41cad043f574b92828d5abe3ae4
Mar <90357336+margrade@users.noreply.github.com>
2022-04-08 14:50:22 -0300

    Delete conversor.py

--- conversor.py
+++
@@ -1,7 +0,0 @@
-pesos=input("¿Cuántos pesos argentinos tienes?:")
-pesos=float(pesos)
-valor_dolar = 212
-dolares=pesos/valor_dolar
-dolares=round(dolares,2)
-dolares=str(dolares)
-print("Tienes $"+dolares+" dólares")
